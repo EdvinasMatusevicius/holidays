c08edc75a7596f02659c4366a3c431b3bb931d7b
Edvinas <edvinas2121@hotmail.com>
2020-10-31 00:44:04 +0200

    added holiday repository methods to controller

--- app/Http/Controllers/HolidaysController.php
+++ app/Http/Controllers/HolidaysController.php
@@ -2,18 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\HolidayRepository;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
 class HolidaysController extends Controller
 {
+    private $holidayRepository;
+
+    public function __construct(HolidayRepository $holidayRepository)
+    {
+        $this->holidayRepository = $holidayRepository;
+    }
     public function getCountryHolidays(Request $request){
         try {
-            $yearHolidays = Http::get("https://kayaposoft.com/enrico/json/v2.0/?action=getHolidaysForYear&year=$request->year&country=$request->countryCode&holidayType=public_holiday")->json();
-            dd($yearHolidays);
+            $holidaysExistsInDb = $this->holidayRepository->checkIfHolidaysInDb($request->year,$request->countryCode,$request->region);
+            if($holidaysExistsInDb){
+                $holidaysFromDb = $this->getHolidaysFromDb($holidaysExistsInDb);
+                //api response
+            }
+            $holidaysFromApi = $this->holidayRepository->getYearHolidays($request->year,$request->countryCode,$request->region);
+            $this->holidayRepository->saveHolidaysToDb($request->year,$request->countryCode,$holidaysFromApi,$request->region);
+            //api response
         } catch (Exception $exception) {
-            
+            //api response
         }
     }
 }
