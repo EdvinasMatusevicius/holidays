b3676134a4e958ae4754f61ad9e841034c488ca6
Edvinas <edvinas2121@hotmail.com>
2020-10-31 02:08:23 +0200

    holiday model and migrationj created

---
+++ database/migrations/2020_10_30_231747_create_holidays_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHolidaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('holidays', function (Blueprint $table) {
+            $table->id();
+            $table->string('year',4);
+            $table->string('countryCode',5);
+            $table->string('region',5)->nullable();
+            $table->text('data');
+            $table->timestamps();
+            $table->index(['year','countryCode','region']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('holidays');
+    }
+}
