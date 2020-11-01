65f878bc2fda7cc5ed92b01b2928d2d95664182b
Edvinas <edvinas2121@hotmail.com>
2020-11-01 05:08:59 +0200

    implementing data parsing functions

--- app/Services/HolidayDataServices.php
+++ app/Services/HolidayDataServices.php
@@ -10,7 +10,32 @@ class HolidayDataServices {
         $regionCode ? $apiUrl .= "&region=$regionCode" : '';
         return Http::get($apiUrl)->json();
     }
-    public function parseHolidayData(array $data){
+    public function parseHolidayData(array $data,string $countryCode,?string $region = null){
+        $totalPublicHolidays = array_reduce($data, "countPublicHolidays", 0);
+    }
+    private function groupByMonth(array $data){
 
+    }
+    private function countPublicHolidays($days,$holiday){
+        if($holiday['holidayType']=='"public_holiday"'){
+            $days +=1;
+        }
+        return $days;
+    }
+    private function getCurrentDayStatus(string $countryCode,?string $region = null){
+        date('D-m-y');
+    }
+    private function calcMaxFreeDaysInRow(array $data){
+
+    }
+    private function isDayWorkDay($day,$month,$year,$countryCode,$region){
+        $apiUrl = "https://kayaposoft.com/enrico/json/v2.0/?action=isWorkDay&date=$day-$month-$year&country=$countryCode";
+        $region ? $apiUrl .= "&region=$region" : '';
+        return Http::get($apiUrl)->json();
+    }
+    private function isDayHoliday($day,$month,$year,$countryCode,$region){
+        $apiUrl = "https://kayaposoft.com/enrico/json/v2.0?action=isPublicHoliday&date=$day-$month-$year&country=$countryCode";
+        $region ? $apiUrl .= "&region=$region" : '';
+        return Http::get($apiUrl)->json();
     }
 }
